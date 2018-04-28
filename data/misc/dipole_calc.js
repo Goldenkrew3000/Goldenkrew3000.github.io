18100a6c496326e987942bd7e84a7fb2c61587ba
Michael <hungrymike@live.com>
2018-04-29 08:25:18 +1000

    changed the javascript a bit - hopefully it will work

--- data/misc/dipole_calc.js
+++ data/misc/dipole_calc.js
@@ -5,7 +5,7 @@ var fullMetre;
 
 var x = "successful";
 
-function dipole_calc (var freq) {
+function dipole_calc(var freq) {
 	//fullFeet = 468 / freq;
 	//fillMetre = 143 / freq;
 	//halfFeet = fullFeet /2;
